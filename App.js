52f1fb292a55e5b631d751791312f2e3897c20d0
Xihai Luo <xihai.luo01@gmail.com>
2022-11-28 15:34:55 -0500

    implement screen to show a post

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import { createStackNavigator } from "react-navigation-stack";
 import { Provider as BlogProvider } from "./src/context/BlogContext";
 
 import IndexScreen from "./src/screens/IndexScreen";
+import ShowScreen from "./src/screens/ShowScreen";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
+    Show: ShowScreen,
   },
   {
     initialRouteName: "Index",
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -10,7 +10,7 @@ import {
 import { Context as BlogContext } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
   return (
@@ -21,14 +21,18 @@ const IndexScreen = () => {
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>
-                {item.title} - {item.id}
-              </Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                <Feather name="trash" style={styles.icon} />
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Show", { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather name="trash" style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           );
         }}
       />
