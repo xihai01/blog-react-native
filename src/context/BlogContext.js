04c7855f59b2b5d4a9cbe9d85f8b8fa346ab67b6
Xihai Luo <xihai.luo01@gmail.com>
2023-03-13 10:43:27 -0400

    ngrok port

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -45,13 +45,15 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({ type: "edit_blogpost", payload: { id, title, content } });
     callback();
   };
